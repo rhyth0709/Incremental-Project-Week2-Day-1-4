7c1168b6e844a80702155931208217d129c68b17
Iamneo <techadmin@iamneo.ai>
2023-11-05 06:33:49 +0000

    Layered Architecture

---
+++ LayerIPL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LayerIPL.Application;
+using LayerIPL.Presentation; 
+namespace LayerIPL;
+public class ConsoleApp
+{
+     public static void Main(string[] args){
+      UserInterface ui =new  UserInterface();
+        ui.Run();
+    } 
+}
